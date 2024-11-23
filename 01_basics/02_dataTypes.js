2cdf1d9de22693a6dc433b2a90f6a572a8c5a7e9
SABA LANGA <112406847+sabalanga06@users.noreply.github.com>
2024-11-23 07:07:42 +0000

    variables

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,6 +3,10 @@ let age = 18
 let isLoggedIn = false
 let state;
 
+
+//alert(3+3) /// by doing this u will get error beacuse inn ode it is not allowed to use directly use alert ... but same has been valid in INSPECT-> CONSOLE in browser ..it depends on engine
+//prefer ECMA and MDN documentation 
+
 //number => 2 to power 53 
 //string
 //Bigint => mostly used fro large values in trading and stock 
@@ -12,4 +16,4 @@ let state;
 //undefined => variable is defined but value is not...u can define it later 
 //object 
 //typeof => to find which type of value is it like string , number or other 
-console.log(typeof null) //o/p is OBJECT 
+console.log(typeof null) //o/p is OBJECT ...null is type of object 
