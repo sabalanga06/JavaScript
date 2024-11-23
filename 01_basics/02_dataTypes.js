b98b91a33934c40f532a27b689089a9d2fc7c7ce
SABA LANGA <112406847+sabalanga06@users.noreply.github.com>
2024-11-23 07:03:56 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "saba"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53 
+//string
+//Bigint => mostly used fro large values in trading and stock 
+//symbol => to define unique values
+//boolean'
+//null=> standalone value 
+//undefined => variable is defined but value is not...u can define it later 
+//object 
+//typeof => to find which type of value is it like string , number or other 
+console.log(typeof null) //o/p is OBJECT 
