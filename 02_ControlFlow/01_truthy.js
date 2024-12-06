859a2d15372c95f786c7ad8d944ef4ee27d0d0b8
SABA LANGA <112406847+sabalanga06@users.noreply.github.com>
2024-12-06 06:54:37 +0000

    js

---
+++ 02_ControlFlow/01_truthy.js
@@ -0,0 +1,44 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+//specially design to handle null and undefined value if it get value as null or defined then for safer side after ?? value is defined as shown in example 10 and 15 will be assigned instead of null and undefined 
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
